9aba75f4ebec413872d3c7d66b7d465a0b134925
Robby Thompson <robsdudeson@gmail.com>
2022-09-29 13:52:06 -0400

    fetch repo labels

--- lib/github/client.ex
+++ lib/github/client.ex
@@ -44,6 +44,19 @@ defmodule ScheduledMerge.Github.Client do
     end
   end
 
+  def fetch_labels(org, repo, url_base, token) do
+    headers = [
+      {"accept", "application/vnd.github+json"},
+      {"authorization", "Bearer #{token}"}
+    ]
+
+    "#{url_base}/repos/#{org}/#{repo}/labels"
+    |> HTTPoison.get!(headers)
+    |> case do
+      %{status_code: 200, body: body} -> Jason.decode!(body)
+    end
+  end
+
   def delete_label(%{"name" => label_name}, org, repo, url_base, token) do
     headers = [
       {"accept", "application/vnd.github+json"},
