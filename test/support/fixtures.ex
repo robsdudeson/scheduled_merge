b3f35d55c430c7efbff6c09f2b8686a7a3b4fe4b
Robby Thompson <robsdudeson@gmail.com>
2022-09-29 15:48:38 -0400

    fix label fixture add pull fixture

--- test/support/fixtures.ex
+++ test/support/fixtures.ex
@@ -4,4 +4,8 @@ defmodule ScheduledMerge.Support.Fixtures do
   def label_fixture(name) do
     %{"name" => name, "color" => Label.default_merge_color()}
   end
+
+  def pull_fixture(labels \\ []) do
+    %{"labels" => labels}
+  end
 end
