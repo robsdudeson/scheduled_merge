8279bd7443a3a054b27dce6ce16cb225e8ea4974
Robby Thompson <robsdudeson@gmail.com>
2022-09-29 14:28:56 -0400

    add error label and remove past_labels from pull

--- lib/github/pull.ex
+++ lib/github/pull.ex
@@ -1,16 +1,6 @@
 defmodule ScheduledMerge.Github.Pull do
   alias ScheduledMerge.Github.Label
 
-  def past?(%{"labels" => labels}, date) do
-    Enum.any?(labels, fn label -> Label.past_merge_label?(label, date) end)
-  end
-
-  def past?(_, _), do: false
-
-  def past_pulls(pulls, date) do
-    Enum.filter(pulls, &past?(&1, date))
-  end
-
   def present?(%{"labels" => labels}, date) do
     Enum.any?(labels, fn label -> Label.present_merge_label?(label, date) end)
   end
