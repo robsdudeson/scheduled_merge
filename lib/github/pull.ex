b479956a355290623f071c79741e0c77581e4e74
Robby Thompson <robsdudeson@gmail.com>
2022-10-05 18:28:19 -0400

    delete today's pulls

--- lib/github/pull.ex
+++ lib/github/pull.ex
@@ -2,13 +2,40 @@ defmodule ScheduledMerge.Github.Pull do
   @moduledoc """
   Functions to mange pulls from GitHub
   """
+
+  require Logger
+
+  import Inject, only: [i: 1]
+
+  alias ScheduledMerge.Github.Client, as: Github
   alias ScheduledMerge.Github.Label
 
-  def present?(%{"labels" => labels}, date) do
-    Enum.any?(labels, fn label -> Label.present_merge_label?(label, date) end)
+  def merge_todays_pulls(date),
+    do:
+      i(GitHub).fetch_pulls()
+      |> present_pulls(date)
+      |> merge_pulls()
+
+  def merge_pulls(pulls) do
+    pulls
+    |> Enum.reduce([], fn pull, errors ->
+      pull
+      |> i(Github).merge_pull()
+      |> case do
+        :ok ->
+          errors
+
+        _ ->
+          Logger.error("Failed to merge pull: '##{pull["number"]}'")
+          [{pull["number"], :pull_merge_error}] ++ errors
+      end
+    end)
   end
 
-  def present_pulls(pulls, date) do
-    Enum.filter(pulls, &present?(&1, date))
-  end
+  def present_pulls(pulls, date), do: Enum.filter(pulls, &present?(&1, date))
+
+  defp present?(%{"labels" => []}, _date), do: false
+
+  defp present?(%{"labels" => labels}, date),
+    do: Enum.any?(labels, fn label -> Label.present_merge_label?(label, date) end)
 end
--- test/github/pull_test.exs
+++ test/github/pull_test.exs
@@ -1,9 +1,35 @@
 defmodule ScheduledMerge.Github.PullTest do
   use ExUnit.Case
 
+  import ExUnit.CaptureLog, only: [capture_log: 1]
+  import Double, only: [stub: 3]
+  import Inject, only: [register: 2]
   import ScheduledMerge.Support.Fixtures
 
+  alias ScheduledMerge.Github.Client, as: Github
   alias ScheduledMerge.Github.Pull
+
+  describe "merge_pulls/1" do
+    setup [:setup_github_client]
+
+    test "it will merge a pull" do
+      pulls = [pull] = [pull_fixture()]
+
+      assert Pull.merge_pulls(pulls) == []
+      assert_received {ScheduledMerge.Github.Client, :merge_pull, [^pull]}
+    end
+
+    @tag merge_pull_result: :error
+    test "it aggregates any errors" do
+      pulls = [pull] = [pull_fixture()]
+
+      assert capture_log(fn ->
+               assert Pull.merge_pulls(pulls) == [{pull["number"], :pull_merge_error}]
+             end) =~ "Failed to merge pull: '##{pull["number"]}'"
+
+      assert_received {ScheduledMerge.Github.Client, :merge_pull, [^pull]}
+    end
+  end
 
   describe "present_pulls/2" do
     test "returns pulls labeled in the past" do
@@ -32,5 +58,19 @@ defmodule ScheduledMerge.Github.PullTest do
 
       assert Pull.present_pulls(pulls, date) == pulls
     end
+  end
+
+  defp setup_github_client(context) do
+    stub =
+      stub(Github, :merge_pull, fn _pull ->
+        case context[:merge_pull_result] do
+          nil -> :ok
+          :error -> {:error, {"a-pull-number", "there was an error merging the pull"}}
+        end
+      end)
+
+    register(Github, stub)
+
+    []
   end
 end
