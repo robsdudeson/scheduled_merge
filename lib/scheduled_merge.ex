79fc5746d6f632dc681d64378f91e5c30ccd055a
Robby Thompson <robsdudeson@gmail.com>
2022-10-05 17:40:30 -0400

    entrypoint is tested

--- lib/scheduled_merge.ex
+++ lib/scheduled_merge.ex
@@ -2,16 +2,33 @@ defmodule ScheduledMerge do
   @moduledoc """
   Documentation for `ScheduledMerge`.
   """
+  import Inject, only: [i: 1]
+
+  alias ScheduledMerge.Github.Label
 
   @doc """
   run the scheduled merge tool
-  ## Examples
 
-      iex> ScheduledMerge.run()
-      :ok
-
+  opts
+    - date: a date used as the 'present' date.  Defaults to `Date.utc_today()`
   """
-  def run() do
-    :ok
+  def run(opts \\ []) do
+    date = date_from_opts(opts)
+
+    label_errors = i(Label).delete_past_labels(date)
+
+    case label_errors do
+      [] -> :ok
+      errors -> {:error, errors}
+    end
+  end
+
+  defp date_from_opts(opts) do
+    opts
+    |> List.keyfind(:date, 0, nil)
+    |> case do
+      nil -> Date.utc_today()
+      string -> Date.from_iso8601!(string)
+    end
   end
 end
