cd0d0c0fbd8b30494e8ab4e039c97343526babb0
Robby Thompson <robsdudeson@gmail.com>
2022-12-06 14:52:25 -0500

    test `merge_pull`

--- test/github/client_test.exs
+++ test/github/client_test.exs
@@ -3,6 +3,7 @@ defmodule ScheduledMerge.Github.ClientTest do
 
   import Double, only: [stub: 3]
   import Inject, only: [register: 2]
+  import ScheduledMerge.Support.Fixtures, only: [pull_fixture: 0]
 
   alias ScheduledMerge.Github.Client
 
@@ -10,24 +11,18 @@ defmodule ScheduledMerge.Github.ClientTest do
   @test_api_url "http://fakeapi.local"
   @test_api_token "bogus_token"
 
+  @success 200
+  @forbidden {403, :forbidden}
+  @not_found {404, :not_found}
+  @method_not_allowed {405, :method_not_allowed}
+  @sha_head_mismatch {409, :sha_head_mismatch}
+  @request_invalid {422, :request_invalid}
+  @server_error {500, :server_error}
+
+  setup [:mock_config]
+
   describe "fetch_pulls/0" do
     setup [:pulls_fetch_stub]
-
-    setup do
-      existing_env = Application.get_env(:scheduled_merge, :github)
-
-      new_env =
-        existing_env
-        |> Enum.into(%{})
-        |> Map.merge(%{repo: @test_repo, api_url: @test_api_url, api_token: @test_api_token})
-        |> Map.to_list()
-
-      Application.put_env(:scheduled_merge, :github, new_env)
-
-      on_exit(fn -> Application.put_env(:scheduled_merge, :github, existing_env) end)
-
-      []
-    end
 
     test "it calls the API with the expected params" do
       Client.fetch_pulls()
@@ -52,15 +47,63 @@ defmodule ScheduledMerge.Github.ClientTest do
     end
   end
 
+  describe "merge_pulls/1" do
+    setup [:pulls_merge_stub]
+
+    setup do
+      [pull: pull_fixture()]
+    end
+
+    test "it calls the API with the proper params", %{pull: %{"number" => pull_number} = pull} do
+      Client.merge_pull(pull)
+
+      assert_received {HTTPoison, :put!, [actual_url, actual_payload, actual_headers]}
+
+      assert actual_url == "#{@test_api_url}/repos/#{@test_repo}/pulls/#{pull_number}/merge"
+      assert actual_payload == ""
+
+      assert actual_headers == [
+               {"accept", "application/vnd.github+json"},
+               {"authorization", "Bearer #{@test_api_token}"}
+             ]
+    end
+
+    test "it deals with http-200s", %{pull: pull} do
+      assert :ok = Client.merge_pull(pull)
+    end
+
+    for {status_code, error_atom} <- [
+          @forbidden,
+          @not_found,
+          @method_not_allowed,
+          @sha_head_mismatch,
+          @request_invalid
+        ] do
+      @tag pulls_merge_response: {:error, status_code}
+      @tag error_atom: error_atom
+      test "it deals with known errors gracefully: #{status_code} #{error_atom}", %{
+        pull: pull,
+        error_atom: error_atom
+      } do
+        assert {:error, error_atom} == Client.merge_pull(pull)
+      end
+    end
+
+    @tag pulls_merge_response: :error
+    test "it blows up for unknown errors", %{pull: pull} do
+      assert_raise CaseClauseError, fn -> Client.merge_pull(pull) end
+    end
+  end
+
   defp pulls_fetch_stub(context) do
     stub =
       stub(HTTPoison, :get!, fn _url, _headers ->
         case context[:pulls_response] do
           nil ->
-            %{status_code: 200, body: pulls_fixture()}
+            %{status_code: @success, body: pulls_fixture()}
 
           :error ->
-            %{status_code: 404, body: "some error"}
+            %{status_code: @server_error, body: "some error"}
         end
       end)
 
@@ -70,4 +113,40 @@ defmodule ScheduledMerge.Github.ClientTest do
   end
 
   defp pulls_fixture, do: File.read!("test/support/fixtures/github_get_pulls.json")
+
+  defp pulls_merge_stub(context) do
+    stub =
+      stub(HTTPoison, :put!, fn _url, _payload, _headers ->
+        case context[:pulls_merge_response] do
+          nil ->
+            %{status_code: @success, body: "huzah!"}
+
+          {:error, code} ->
+            %{status_code: code, body: "some error"}
+
+          :error ->
+            %{status_code: @server_error, body: "server error"}
+        end
+      end)
+
+    register(HTTPoison, stub)
+
+    []
+  end
+
+  defp mock_config(_context) do
+    existing_env = Application.get_env(:scheduled_merge, :github)
+
+    new_env =
+      existing_env
+      |> Enum.into(%{})
+      |> Map.merge(%{repo: @test_repo, api_url: @test_api_url, api_token: @test_api_token})
+      |> Map.to_list()
+
+    Application.put_env(:scheduled_merge, :github, new_env)
+
+    on_exit(fn -> Application.put_env(:scheduled_merge, :github, existing_env) end)
+
+    []
+  end
 end
